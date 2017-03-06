3d6d18d13e01cf463a03c66fdb6c9825910566fd
Nathan Struhs <nathanstruhs@gmail.com>
2017-03-06 11:46:25 -0800

    Didnt really do much.. sorry brad.. 😔

---
+++ public/scripts/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+	var = $("#vegetables-checkbox");
+	var = $("#plants-checkbox");
+	var = $("#ungulates-checkbox");
+	var = $("#terrorist-checkbox");
+	var = $("#murderers-checkbox");
+	var = $();
+	var = $();
+	var = $();
+	var = $();
+	var = $();
+})
+
+
+
+ 
+
