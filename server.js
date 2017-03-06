bd017a7a9081f5cb6ffd1824a780853aeec94cb4
Bradley Cook <bradleycook@10-116-35-31.wireless.oregonstate.edu>
2017-03-06 11:33:10 -0800

    added listening

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function(req,res){
-	res.sendfile(path.join(_dirname + '/public/index.html'));
+	res.sendfile(path.join(__dirname + '/public/index.html'));
+});
+
+var server = app.listen(8080, function() {
+	console.log("server listening at 8080");
 });
