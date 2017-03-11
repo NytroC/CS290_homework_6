b7f620e0bd78bb513a238cf4dd64335a41d57001
Nathan Struhs <nathanstruhs@gmail.com>
2017-03-11 10:21:45 -0800

    Populate teams

--- public/scripts/jeopardy.js
+++ public/scripts/jeopardy.js
@@ -1,47 +1,36 @@
-// var app = angular.module("myApp", []);
-// var categories = ["A","B"];
-// $(document).ready(function() {
-	
-// 	$.post("/get_game_info").done(function(data) {
-		
-		
-// 		for (var key in data) {
-// 			if (data[key] == "true") {
-// 				categories.push(key);
-// 			}
-// 		}
-// 		console.log(categories);
-
-// 		var teams = [];
-// 		for (var key in data) {
-// 			if (data[key] != "" && data[key] != "true" && data[key] != "false") {
-// 				teams.push(data[key]);
-// 			}
-// 		}
-// 		console.log(teams);
-// 		console.log("Success, game info received");
-
-// 	}).fail(function() {	
-// 		console.log("Get game info failed");
-// 	});
-
-// });
 var app = angular.module('myApp', []);
-	var categories = [];
-	app.controller('myCtrl', function($scope, $http) {
-	    $http({
-	        method : "POST",
-	        url : "/get_game_info"
-	    }).then(function mySucces(response) {
-	        $scope.data = response.data;
-	        $scope.categories = []
-	        for (var key in $scope.data) {
-				if ($scope.data[key] == "true") {
-					$scope.categories.push(key);
-					console.log(key);
-				}
+var categories = [];
+app.controller('game_controller', function($scope, $http) {
+    $http({
+        method : "POST",
+        url : "/get_game_info"
+    }).then(function mySucces(response) {
+        $scope.data = response.data;
+        $scope.categories = []
+        for (var key in $scope.data) {
+			if ($scope.data[key] == "true") {
+				$scope.categories.push(key);
+				console.log(key);
 			}
-	    }, function myError(response) {
-	       console.log("failed");
-	    });
-	});
+		}
+
+        $scope.data = response.data;
+        $scope.teams = []
+        for (var key in $scope.data) {
+            if ($scope.data[key] != "" && $scope.data[key] != "true" && $scope.data[key] != "false") {
+                $scope.teams.push($scope.data[key]);
+            }
+        }
+    }, function myError(response) {
+       console.log("failed");
+    });
+});
+
+
+
+
+
+
+
+
+
