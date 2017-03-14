8e8ee799f4a1f2d7d04bad4ab14dfec30333f77f
Nathan Struhs <nathanstruhs@gmail.com>
2017-03-13 19:29:06 -0700

    Finished

--- public/scripts/jeopardy.js
+++ public/scripts/jeopardy.js
@@ -40,28 +40,37 @@ app.controller('game_controller', function($scope, $http) {
         $scope.column_4 = $scope.questions[3];
         $scope.column_5 = $scope.questions[4];
 
-        $scope.click = function(category, index, column_index) {
+        $scope.point_value;
+
+        $scope.click = function($event,category, index, column_index) {
             console.log("cat: " + category + " index: " + index + " c_i: " + column_index);
+            $($event.currentTarget).css("text-decoration", "line-through");
             alert(category + " " + index + " " + column_index);
             $("#question_container").css("display", "block");
             if (category == 'vegetables') {
                 $("#question").html(JSON.stringify($scope.questions[column_index].vegetables[index][0]));
                 $("#answer").html(JSON.stringify($scope.questions[column_index].vegetables[index][1]));
+                $scope.point_value = (index + 1) * 100;
             }  if (category == 'plants') {
                 $("#question").html(JSON.stringify($scope.questions[column_index].carnivorous_plants[index][0]));
                 $("#answer").html(JSON.stringify($scope.questions[column_index].carnivorous_plants[index][1]));
+                $scope.point_value = (index + 1) * 100;
             }  if (category == 'terrorists') {
                 $("#question").html(JSON.stringify($scope.questions[column_index].terrorist_attacks[index][0]));
                 $("#answer").html(JSON.stringify($scope.questions[column_index].terrorist_attacks[index][1]));
+                $scope.point_value = (index + 1) * 100;
             }  if (category == 'ungulates') {
                 $("#question").html(JSON.stringify($scope.questions[column_index].ungulates[index][0]));
                 $("#answer").html(JSON.stringify($scope.questions[column_index].ungulates[index][1]));
+                $scope.point_value = (index + 1) * 100;
             }  if (category == 'chickens') {
                 $("#question").html(JSON.stringify($scope.questions[column_index].chickens[index][0]));
                 $("#answer").html(JSON.stringify($scope.questions[column_index].chickens[index][1]));
+                $scope.point_value = (index + 1) * 100;
             }  if (category == 'murderers') {
                 $("#question").html(JSON.stringify($scope.questions[column_index].mass_murderers[index][0]));
                 $("#answer").html(JSON.stringify($scope.questions[column_index].mass_murderers[index][1]));
+                $scope.point_value = (index + 1) * 100;
             }
         }
 
@@ -70,11 +79,20 @@ app.controller('game_controller', function($scope, $http) {
         })
 
         $scope.scores = [];
+        console.log("scores="+$scope.scores);
         for (var j = 0; j < $scope.teams.length; j++) {
-            $scope.scores.push(j);
+            $scope.scores.push(0);
         }
 
     });
+
+    $scope.update_score = function($index) {
+        console.log("updating scoreeee");
+        $scope.scores[$index] += $scope.point_value;
+        alert("Points!");
+        $("#answer").css("display", "none");
+        $("#question_container").css("display", "none");
+    }
 
 });
 
